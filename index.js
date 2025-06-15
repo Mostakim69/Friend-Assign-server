9116821700b903b8266e5da99be950e330542755
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-06-16 02:27:10 +0600

    Post Api

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxrw3xt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,24 +21,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const db = client.db('assignmentDB');
+    const assignmentsCollection = db.collection('assignments');
+
+    // Endpoint to create a new assignment
+    app.post('/api/assignments', async (req, res) => {
+      const assignment = req.body;
+      const result = await assignmentsCollection.insertOne(assignment);
+      res.status(201).send(result);
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+  res.send('Assignment Code Cooking');
+});
 
-app.get('/', (req, res) =>{
-    res.send('Assignment Code Cooking')
-})
-
-
-app.listen(port, () =>{
-    console.log(`Assignment code server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Assignment code server is running on port ${port}`);
+});
